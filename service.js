7e752fd33a6cbba6499015a47d3dfc3616548300
SECHUBSYSTEM <106301523+SECHUBSYSTEM@users.noreply.github.com>
2022-08-16 21:52:06 +0100

    Update service.js

--- service.js
+++ service.js
@@ -1,10 +1,49 @@
-const package = document.querySelector('.package');
-const choose = document.querySelector('.show');
+let count = 0;
+const valueDisplay = document.getElementById("valueDisplay");
+const package = document.querySelector('#package');
+const show = document.querySelector('#show');
 
 package.addEventListener('click', function () {
     showPackage();
 })
-
+show.addEventListener('fadeIn', function () {
+    showPackage();
+})
 function showPackage() {
-    choose.classList.toggle('hidden');
+    show.classList.toggle('hidden');
+    show.classList.fadeIn('hidden');
+
 }
+
+function increment() {
+    if (count < 7) {
+        count++;
+    }
+    valueDisplay.innerHTML = count;
+};
+
+
+function decrement() {
+    if (count > 0) {
+        count--;
+    }
+    valueDisplay.innerHTML = count;
+};
+
+let read = 0;
+const value1 = document.getElementById("value1");
+
+function increase() {
+    if (read < 7) {
+        read++;
+    }
+    value1.innerHTML = read;
+};
+
+
+function decrease() {
+    if (read > 0) {
+        read--;
+    }
+    value1.innerHTML = read;
+};
